9cf8ffd483628179ecc7d6a5a8b500e0ea774ce5
Hossein Mehrmohammadi <48206682+HosseinMehrmohammadi@users.noreply.github.com>
2021-05-06 15:22:20 -0700

    Q4

---
+++ Assignment_2/multiplier_4bit.v
@@ -0,0 +1,90 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    23:42:20 05/06/2021 
+// Design Name: 
+// Module Name:    multiplier_4bit 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module multiplier_4bit(
+		input [3:0] a,
+		input [3:0] b,
+		
+		output [7:0] r
+    );
+		
+		and(r[0], a[0], b[0]);
+		
+		wire [3:0] w0;
+		and(w0[0], a[0], b[1]);
+		and(w0[1], a[0], b[2]);
+		nand(w0[2], a[0], b[3]);
+		assign w0[3] = 1;
+		
+		wire [3:0] w1;
+		and(w1[0], a[1], b[0]);
+		and(w1[1], a[1], b[1]);
+		and(w1[2], a[1], b[2]);
+		nand(w1[3], a[1], b[3]);
+		
+		wire [4:0] w2;
+		
+		ripple_adder_dataflow rad1 (
+			.a (w0),
+			.b (w1),
+			.cin (0),
+			.sum (w2[3:0]),
+			.cout (w2[4])
+		);
+		
+		assign r[1] = w2[0];
+		
+		wire [3:0] w3;
+		and(w3[0], a[2], b[0]);
+		and(w3[1], a[2], b[1]);
+		and(w3[2], a[2], b[2]);
+		nand(w3[3], a[2], b[3]);
+		
+		wire [4:0] w4;
+		
+		ripple_adder_dataflow rad2 (
+			.a (w2[4:1]),
+			.b (w3),
+			.cin (0),
+			.sum (w4[3:0]),
+			.cout (w4[4])
+		);
+		
+		assign r[2] = w4[0];
+		
+		wire [3:0] w5;
+		nand(w5[0], a[3], b[0]);
+		nand(w5[1], a[3], b[1]);
+		nand(w5[2], a[3], b[2]);
+		and(w5[3], a[3], b[3]);
+
+		wire w6;
+		
+		ripple_adder_dataflow rad3 (
+			.a (w4[4:1]),
+			.b (w5),
+			.cin (0),
+			.sum (r[6:3]),
+			.cout (w6)
+		);
+		
+		xor(r[7], w6, 1);
+		
+endmodule
---
+++ Assignment_2/multiplier_4bit_testbench.v
@@ -0,0 +1,53 @@
+`timescale 1ns / 1ps
+
+////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer:
+//
+// Create Date:   00:51:00 05/07/2021
+// Design Name:   multiplier_4bit
+// Module Name:   D:/Educational Files/Digital Systems/cad-master/Assignment_2/Q4_Q5/multiplier_4bit_testbench.v
+// Project Name:  Q4_Q5
+// Target Device:  
+// Tool versions:  
+// Description: 
+//
+// Verilog Test Fixture created by ISE for module: multiplier_4bit
+//
+// Dependencies:
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+////////////////////////////////////////////////////////////////////////////////
+
+module multiplier_4bit_testbench;
+
+	// Inputs
+	reg [3:0] a;
+	reg [3:0] b;
+
+	// Outputs
+	wire [7:0] r;
+
+	// Instantiate the Unit Under Test (UUT)
+	multiplier_4bit uut (
+		.a(a), 
+		.b(b), 
+		.r(r)
+	);
+
+	integer i, j;
+	initial begin
+		for (i=0; i<16; i=i+1) begin
+			a = i;
+			for (j=0; j<16; j=j+1) begin
+				b = j;
+				#3;
+			end
+		end
+	end
+      
+endmodule
+
